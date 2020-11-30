8943da94a5a4a6d8508fe50e36efca01057072b1
Anna <olejnikanuta@gmail.com>
2020-11-30 23:23:30 +0100

    First commit

---
+++ app/src/main/java/com/example/androidapp/java/AddSkillEmployer.java
@@ -0,0 +1,17 @@
+package com.example.androidapp.java;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidapp.R;
+
+public class AddSkillEmployer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_skill_employer);
+
+    }
+}
